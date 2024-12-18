74348d5a99704ae09e676e51e1ffcbbcbd7ff165
Jinho Lee <ruinbible@ezcaretech.com>
2024-12-18 22:19:36 +0900

    점프 액션 기능 추가

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -12,6 +12,23 @@ class GamePage extends StatefulWidget {
 class _GamePageState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
-    return GameWidget(game: MyGame());
+    return GameWidget(
+      game: MyGame(),
+      overlayBuilderMap: {
+        'touchButton': (BuildContext context, MyGame game) {
+          return Align(
+            alignment: Alignment.center,
+            child: Listener(
+              behavior: HitTestBehavior.translucent,
+              onPointerDown: (event) => game.jump(),
+              child: SizedBox(
+                width: game.size.x,
+                height: game.size.y,
+              ),
+            ),
+          );
+        },
+      },
+    );
   }
 }
